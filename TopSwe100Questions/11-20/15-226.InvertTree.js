ea798675725e2c4c267d91c51df3d4062f0e54d9
Miketuazon <michael.d.tuazon@gmail.com>
2024-04-09 23:59:33 -0400

    tree 15;

---
+++ TopSwe100Questions/11-20/15-226.InvertTree.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+    if (node !== null) {
+        const tmp = node.left;
+        node.left = invertTree(node.right);
+        node.right = invertTree(tmp);
+    }
+    return node;
+};
